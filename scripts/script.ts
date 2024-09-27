440f8f189abe192de6d5e802a89b85f70ec075be
Arsen Aleksanyan <arsen.aleksanyan.1994@gmail.com>
2024-09-27 13:51:33 +0400

    Update modal structure per design

--- scripts/script.ts
+++ scripts/script.ts
@@ -75,11 +75,24 @@ function createGridItem(beer: Beer): HTMLElement {
 // Show the modal with the selected beer's details
 function showModal(beer: Beer): void {
   const modal = document.getElementById("modal")!;
+  const ibuText = document.getElementById("ibu-text") as HTMLElement;
+  const abvText = document.getElementById("abv-text") as HTMLElement;
+  const beerImage = document.getElementById(
+    "modal-beer-image"
+  ) as HTMLImageElement;
 
-  document.getElementById("modal-title")!.textContent = beer.name;
-  document.getElementById("modal-description")!.textContent = beer.description;
+  if (modal) {
+    (document.getElementById("modal-title") as HTMLElement).textContent =
+      beer.name;
+    (document.getElementById("modal-description") as HTMLElement).textContent =
+      beer.description;
 
-  modal.style.display = "block";
+    ibuText.innerHTML = `IBU <br> ${beer.ibu}`;
+    abvText.textContent = `${beer.abv}%`;
+    beerImage.src = beer.image_url;
+
+    modal.style.display = "block";
+  }
 }
 
 // Hide the modal
